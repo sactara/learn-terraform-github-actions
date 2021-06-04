fb80c8903c359339d131753243a6aa1b646dbc78
Jiten <jitendra.tarabadkar@hotmail.com>
2021-06-04 19:18:41 +0530

    Point to corect TFC org, workspace and changed resources

--- main.tf
+++ main.tf
@@ -12,45 +12,25 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "GHTF"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "git-terra-actions"
     }
   }
 }
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 
 
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_instance" "NewMediuminstance" {
+  ami = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.medium"
+  tags = {
+    Name = "EC2 Instance Test"
   }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
